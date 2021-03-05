e427baafea887a0ca5f1471bd344fa6df2f5ec0a
Guilherme Domke de Thomaz <gthomaz@uolinc.com>
2021-03-05 19:44:58 -0300

    GuilhermeDomkeDeThomaz-Inclusão de contatos

--- App.js
+++ App.js
@@ -1,21 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Text, TextInput, StyleSheet, Button, View, FlatList } from 'react-native';
 
 export default function App() {
-  return (
+  const [nome, setNome] = useState('');
+  const capturarNome = (nome) => {setNome(nome)};
+  const [telefone, setTelefone] = useState('');
+  const capturarTelefone = (telefone) => {setTelefone(telefone)};
+  const [contatos, setContatos] = useState([]);
+  const [contadorContatos, setContadorContatos] = useState(0);
+  const adicionarContato = () => {
+    setContatos(() => {
+      console.log('Nome: ' + nome, ' Telefone: ' + telefone);
+      let contato = {
+        'nome': nome,
+        'telefone': telefone
+      }
+      setContadorContatos(contadorContatos + 1);
+      return [...contatos, {key: contadorContatos.toString(), value: contato}];
+    });
+  }
+
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.contatoInputView}>
+        <TextInput
+          placeholder="Digite o nome..."
+          style={styles.contatoTextInput}
+          onChangeText={capturarNome}
+          value={nome}
+        />
+        <TextInput
+          placeholder="Digite o telefone..."
+          style={styles.contatoTextInput}
+          onChangeText={capturarTelefone}
+          value={telefone}
+        />
+        <Button
+          style={styles.contatoInputButton}
+          title="Inserir Contato"
+          onPress={adicionarContato}
+        />
+      </View>
+      <View style={styles.contatosFlatList}>
+        <FlatList
+          data={contatos}
+          renderItem={
+            contato => (
+              <View style={styles.contatoNaLista}>
+                <Text>{contato.item.value.nome}</Text>
+              </View>
+            )
+          }
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50,
+    flex: 1
   },
+  contatoInputView: {
+    alignItems: 'center',
+    marginBottom: 12
+  },
+  contatoTextInput: {
+    width: '80%',
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    marginBottom: 4,
+    padding: 2,
+    textAlign: 'center'
+  },
+  contatoInputButton: {
+    width: '80%'
+  },
+  contatoNaLista: {
+    padding: 12,
+    backgroundColor: '#CCC',
+    borderColor: 'black',
+    borderWidth: 1,
+    marginBottom: 8,
+    borderRadius: 8,
+    alignItems: 'center'
+  },
+  contatosFlatList: {
+    width: '80%',
+    alignSelf: 'center'
+  }
 });
